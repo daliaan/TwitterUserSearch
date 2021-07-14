26b26f4dab31989141f037df38c72c2199d052d5
Dalian Razvan Cucer <R.Cucer@printecgroup.com>
2021-07-14 08:35:44 +0300

    repositoryImpl tests added

--- app/src/main/java/dalian/razvan/cucer/twitterusersearch/core/repository/models/TwitterUser.kt
+++ app/src/main/java/dalian/razvan/cucer/twitterusersearch/core/repository/models/TwitterUser.kt
@@ -4,15 +4,26 @@ import com.google.gson.annotations.SerializedName
 import dalian.razvan.cucer.twitterusersearch.utils.Constants
 import org.json.JSONObject
 
-data class TwitterUser(@SerializedName(Constants.ID) val id: String,
-                       @SerializedName(Constants.NAME) val name: String,
-                       @SerializedName(Constants.USERNAME) val username: String,
-                       @SerializedName(Constants.PROFILE_PICTURE) val profilePicture: String) {
+data class TwitterUser(@SerializedName(Constants.ID) val id: String = "",
+                       @SerializedName(Constants.NAME) val name: String = "",
+                       @SerializedName(Constants.USERNAME) val username: String = "",
+                       @SerializedName(Constants.PROFILE_PICTURE) val profilePicture: String = "") {
     constructor(jsonObject: JSONObject):
             this(
-                jsonObject.optString(Constants.ID),
-                jsonObject.optString(Constants.NAME),
-                jsonObject.optString(Constants.USERNAME),
-                jsonObject.optString(Constants.PROFILE_PICTURE)
-            )
+                jsonObject.optString(Constants.ID)?:"",
+                jsonObject.optString(Constants.NAME)?:"",
+                jsonObject.optString(Constants.USERNAME)?:"",
+                jsonObject.optString(Constants.PROFILE_PICTURE)?:""
+            ) {
+                this.jsonObject = jsonObject
+            }
+    private var jsonObject = JSONObject()
+
+    override fun toString(): String {
+        return jsonObject.toString()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other.toString() == toString()
+    }
 }
---
+++ app/src/test/java/dalian/razvan/cucer/twitterusersearch/core/repository/RepositoryImplTest.kt
@@ -0,0 +1,48 @@
+package dalian.razvan.cucer.twitterusersearch.core.repository
+
+import dalian.razvan.cucer.twitterusersearch.core.networking.API
+import dalian.razvan.cucer.twitterusersearch.core.networking.APICallbackInterface
+import dalian.razvan.cucer.twitterusersearch.core.networking.error.ErrorCallback
+import dalian.razvan.cucer.twitterusersearch.core.repository.models.TwitterUser
+import dalian.razvan.cucer.twitterusersearch.screens.search.list.TwitterUsersCallback
+import dalian.razvan.cucer.twitterusersearch.utils.Constants
+import kotlinx.coroutines.runBlocking
+import org.json.JSONArray
+import org.json.JSONObject
+import org.junit.*
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
+
+class RepositoryImplTest {
+    @Mock
+    lateinit var user1: TwitterUser
+    @Mock
+    lateinit var user2: TwitterUser
+    @Mock
+    lateinit var repository: RepositoryImpl
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        user1 = TwitterUser(mock(JSONObject::class.java))
+        user2 = TwitterUser(mock(JSONObject::class.java))
+        repository = RepositoryImpl(mock(API::class.java))
+    }
+
+    @After
+    fun tearDown() {}
+
+    @Test
+    fun data_storeSelectedUser_pass() {
+        repository.setSelectedUser(user1)
+        Assert.assertEquals(user1, repository.getSelectedUser())
+    }
+
+    @Test
+    fun data_storeSelectedUser_doesntPass() {
+        repository.setSelectedUser(user1)
+        Assert.assertNotEquals(user2, repository.getSelectedUser())
+    }
+}
